9f18e27a884c6eff81e49ce768b81c307f7178f8
chen.chen01 <chen.chen01@bianlifeng.com>
2018-12-15 20:06:56 +0800

    Add-app-添加首页文件

---
+++ src/pages/main/components/OrderPage/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="container">
+        首页
+      </div>
+    );
+  }
+}
+
+export default HomePage;
---
+++ src/pages/main/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div className="container">
+        首页
+      </div>
+    );
+  }
+}
+
+export default MainPage;
