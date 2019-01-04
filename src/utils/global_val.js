4f27bce9d756fed913c3b44512ec3934b99fcfe1
jun.zhang <jun.ju.zhang@oracle.com>
2019-01-04 15:44:49 +0800

    fix

--- src/utils/global_val.js
+++ src/utils/global_val.js
@@ -28,7 +28,7 @@ const globalVal = {
     }
     return JSON.parse(userInfo);
   },
-  imgUrl: 'http://admin.offline.ue-link.com/static/',
+  imgUrl: 'http://admin.offline.ue-link.cn/static/',
   //imgUrl: 'http://148.70.110.127:8080/static/',
 
   routeIsFromPay: null,
