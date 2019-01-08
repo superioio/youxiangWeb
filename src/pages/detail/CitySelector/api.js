6e2346832013fd2426eb68b7488432e6678b1fc1
jun.zhang <jun.ju.zhang@oracle.com>
2019-01-08 11:55:12 +0800

    地址获取二级城市

--- src/pages/detail/CitySelector/api.js
+++ src/pages/detail/CitySelector/api.js
@@ -2,7 +2,11 @@ import axios from '@/utils/http'
 
 //获取城市列表
 export async function getCityList() {
-  return await axios.get('/api/syscity/getprovincelist')
+  return await axios.get('/api/syscity/getcitylist',{
+          params: {
+              cityCode: 'all',
+          }
+  })
     .then(function (response) {
       return response;
     })
